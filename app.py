78fc34cd144fedad9497c5acd5c9a641fec1cdeb
Oleg <oleg.a.matyash@gmail.com>
2022-06-30 06:45:49 +0300

    add

--- app.py
+++ app.py
@@ -1,16 +1,61 @@
 import os
+import re
+from typing import Iterator
 
-from flask import Flask
+from flask import Flask, request, Response
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
+def slice_limit(it: Iterator, value: int) -> Iterator:
+    i = 0
+    for item in it:
+        if i < value:
+            yield item
+        else:
+            break
+        i += 1
 
-@app.post("/perform_query")
-def perform_query():
-    # нужно взять код из предыдущего ДЗ
-    # добавить команду regex
-    # добавить типизацию в проект, чтобы проходила утилиту mypy app.py
-    return app.response_class('', content_type="text/plain")
+def build_query(it: Iterator, cmd: str, value: str) -> Iterator:
+    res = map(lambda v: v.strip(), it)
+    if cmd == 'filter':
+        return filter(lambda v: value in v, res)
+    if cmd == 'sort':
+        return iter(sorted(res, reverse=bool(value)))
+    if cmd == 'unique':
+        return iter(set(res))
+    if cmd == 'limit':
+        return slice_limit(res, int(value))
+    if cmd == 'map':
+        return map(lambda v: v.split(' ')[int(value)], res)
+    if cmd == 'regex':
+        regex = re.compile(value)
+        return filter(lambda x: regex.search(x), res)
+    return res
+
+@app.route("/perform_query", methods=['POST'])
+def perform_query() -> Response:
+    try:
+        if request.args.get('cmd_2'):
+            print("yes")
+        cmd_1 = request.args['cmd_1']
+        cmd_2 = request.args['cmd_2']
+        val_1 = request.args['val_1']
+        val_2 = request.args['val_2']
+        file_name = request.args['file_name']
+    except:
+        raise BadRequest
+
+    path_file = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(path_file):
+        raise BadRequest(description=f"{file_name} was not found")
+
+    with open(path_file) as f:
+        res = build_query(f, cmd_1, val_1)
+        res = build_query(res, cmd_2, val_2)
+        content = '\n'.join(res)
+
+    return app.response_class(content, content_type="text/plain")
---
+++ try.py
@@ -0,0 +1,6 @@
+id = ['1', '1', '1']
+
+def change(x):
+    return int(x)
+
+print(list(map(change, id)))
